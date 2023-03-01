8830502dafc28dafb59ac670f9965a345a5dbb43
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-03-01 13:27:45 +0200

    changed board size to be bigger

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-export const size = 30;
+export const size = 50;
 export const tick = 100;
 export const svgContainer = document.getElementById("svgContainer");
 export const eatPoint = document.getElementById("eatPoint");
